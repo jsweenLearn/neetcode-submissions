ff0e6fe7f2b545948738b02240197cc7c1b56a66
jsweenLearn <jon.learn.code@gmail.com>
2026-05-08 10:08:02 -0700

    Add: two-integer-sum - submission-5

---
+++ Data Structures & Algorithms/two-integer-sum/submission-5.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = nums.Length - 1; j > i; j--)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    return [i, j];
+                }
+            }
+        }
+        return [];
+    }
+}
