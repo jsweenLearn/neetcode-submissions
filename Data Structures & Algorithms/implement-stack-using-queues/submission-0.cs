d121f7860ed664b0551663295dad7a1d5429e747
jsweenLearn <jon.learn.code@gmail.com>
2026-05-16 09:29:44 -0700

    Add: implement-stack-using-queues - submission-0

---
+++ Data Structures & Algorithms/implement-stack-using-queues/submission-0.cs
@@ -0,0 +1,40 @@
+public class MyStack {
+    Queue<int> q1;
+    Queue<int> q2;
+
+    public MyStack() {
+        q1 = new();
+        q2 = new();
+    }
+    
+    public void Push(int x) {
+        q2 = q1;
+        q1 = new();
+        q1.Enqueue(x);
+        foreach( int n in q2)
+        {
+            q1.Enqueue(n);
+        }
+    }
+    
+    public int Pop() {
+        return q1.Dequeue();
+    }
+    
+    public int Top() {
+        return q1.Peek();
+    }
+    
+    public bool Empty() {
+        return q1.Count == 0;
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.Push(x);
+ * int param_2 = obj.Pop();
+ * int param_3 = obj.Top();
+ * bool param_4 = obj.Empty();
+ */
