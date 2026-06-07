ac14df72328002ab44c8bb5dfb3f8c8b80086873
jsweenLearn <jon.learn.code@gmail.com>
2026-06-07 09:05:41 -0700

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 1
+print(type(variable))
+variable = 1.1
+print(type(variable))
+variable = False
+print(type(variable))
+variable = "23r4"
+print(type(variable))
+variable = [1, 2]
+print(type(variable))
